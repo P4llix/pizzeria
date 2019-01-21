b6557ee7b73b5219b8a576a8ec7e46fda2c3adcf
P4llix <45855475+P4llix@users.noreply.github.com>
2019-01-21 21:03:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function czas() {
+    var data = new Date();
+    var godzina = data.getHours();
+    var minuta = data.getMinutes();
+    var sekunda = data.getSeconds();
+
+    if (godzina < 10) {
+        godzina = "0" + godzina;
+    }
+    if (minuta < 10) {
+        minuta = "0" + minuta;
+    }
+    if (sekunda < 10) {
+        sekunda = "0" + sekunda;
+    }
+
+    setTimeout(czas, 1000);
+    document.getElementById("zegar").innerHTML = godzina + " : " + minuta + " : " + sekunda;
+
+}
+window.addEventListener("load", czas);
+
+var glicz = document.getElementById("guzik-licz");
+glicz.addEventListener("click",function() {
+    document.getElementById("wynik").innerHTML = "";
+    var liczba1 = document.getElementById("odleglosc").value;
+
+if(liczba1){
+    if (liczba1 <= 3){
+        var koszt = "Darmowa dostawa";
+    }
+    else if (liczba1 >= 3 && liczba1 <= 10){
+        var koszt = "Koszt wynosi 5zł";
+    }
+    else if (liczba1 > 10){
+        var koszt = "Koszt wynosi 10zł";
+    }
+    document.getElementById("wynik").innerHTML = "<p id='koszt'>" + koszt + "</p>";
+}
+})
+
+function wysuwanie(){
+    if(document.getElementById("liczenie").style.opacity = "0"){
+        document.getElementById("liczenie").style.opacity = "1";
+    }
+}
+
+function wygasz() {
+    document.getElementById("liczenie").style.opacity = "0";
+    document.getElementById("koszt").style.opacity = "0";
+
+}
